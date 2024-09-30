b54bcb4062d4d2084cf34585c5797c2619b9f391
eid <eidhachem1@gmail.com>
2024-09-30 10:42:52 +0300

    Add gsap from method

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,19 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      duration: 5,
+      ease: "power1.inOut",
+    })
+  }, [])
 
   return (
     <main>
@@ -34,7 +48,7 @@ const GsapFrom = () => {
         <div id="green-box" className="w-20 h-20 bg-green-500 rounded-lg" />
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default GsapFrom;
+export default GsapFrom
